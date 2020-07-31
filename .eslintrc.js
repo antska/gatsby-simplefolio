8b2a5f81caab9508ef5dad96e6c054a30f4f056c
Antonis Skandalis <ant.skandalis@gmail.com>
2020-07-31 20:43:18 +0300

    chore: add configuration for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,36 @@
 module.exports = {
-  extends: ['airbnb', 'prettier'],
-  plugins: ['prettier'],
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'airbnb',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'react'],
   env: {
+    node: true,
     browser: true,
     es6: true,
   },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
     'import/no-extraneous-dependencies': 'off',
     'prettier/prettier': 'error',
     'no-console': 'warn',
@@ -14,6 +39,15 @@ module.exports = {
     'react/static-property-placement': 'off', // disable if necessary
     'react/jsx-props-no-spreading': 'off', // disable if necessary
     'react/require-default-props': 'off',
+    'react/prop-types': 'off',
   },
-  parser: 'babel-eslint',
+  overrides: [
+    // Override some TypeScript rules just for .js files
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off', //
+      },
+    },
+  ],
 };
